3ef54333ab6a4a64bba9be367ad5759452867eb9
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-04-19 11:38:41 +0200

    Improved: generate a random honeypot name every 24 hrs. Added: brevo_honeypot_expiration filter

--- mailin-honeypot.php
+++ mailin-honeypot.php
@@ -15,15 +15,35 @@ class BrevoHoneypot {
 	private $honeypot_name;
 
 	/**
+	 * Build class.
 	 *
+	 * @throws Exception
 	 */
 	public function __construct() {
-		$this->honeypot_name = apply_filters( 'brevo_honeypot_name', 'come_and_get_it' );
+		$this->honeypot_name = $this->get_honeypot_name();
 
 		/**
 		 * Run before 'mailin', which runs at default priority (10).
 		 */
 		add_action( 'init', [ $this, 'init' ], 9 );
+	}
+
+	/**
+	 * Generates a new name value every 24 hrs.
+	 *
+	 * @return mixed|null
+	 * @throws Exception
+	 */
+	private function get_honeypot_name() {
+		$name = get_transient( 'mailin_honeypot_name' );
+
+		if ( ! $name ) {
+			$name = bin2hex( random_bytes( 4 ) );
+
+			set_transient( 'mailin_honeypot_name', $name, apply_filters( 'brevo_honeypot_expiration', 86400 ) );
+		}
+
+		return apply_filters( 'brevo_honeypot_name', $name );
 	}
 
 	/**
